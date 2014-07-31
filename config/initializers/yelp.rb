d46029ff9014a618a4aca5110a6bd848510c8d75
Vanessa Dean <vanessa.dean@gmail.com>
2014-07-31 07:26:55 -0400

    Add restaurant resource

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,14 +1,6 @@
-require 'yelp'
-
 Yelp.client.configure do |config|
   config.consumer_key = 'h6izpzzte-6P3SeJ3uluZg'
   config.consumer_secret = 'Ks7ZLrcurboT8GidbkMT4oDBwoM'
   config.token = 'F-EgdJc44BZhoq3FcwsrMDFGdJHL7aUg'
   config.token_secret = 'N5d15VGLriVUkO3n1DbSp59O8gY'
 end
-
-# Yelp.configure(:yws_id          => 'rFmcGPL25j585PrcQHlIZQ',
-#                :consumer_key    => 'h6izpzzte-6P3SeJ3uluZg',
-#                :consumer_secret => 'Ks7ZLrcurboT8GidbkMT4oDBwoM',
-#                :token           => 'F-EgdJc44BZhoq3FcwsrMDFGdJHL7aUg',
-#                :token_secret    => 'N5d15VGLriVUkO3n1DbSp59O8gY')
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140731112050) do
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "image_url"
+    t.string   "rating"
+    t.string   "rating_img_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
