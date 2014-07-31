01fc9238784d0b3f54406d56d2fe2064d54762c0
kriscroes <k_croes@hotmail.com>
2014-07-31 13:05:58 -0400

    add distance

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
+require'pry'
+
 class Restaurant < ActiveRecord::Base
     
   def self.get_yelp(lat, lon)
@@ -11,6 +13,7 @@ class Restaurant < ActiveRecord::Base
     #searching
     search = Yelp.client.search_by_coordinates(coordinates, params, locale) 
 
+
     search.businesses.each do |restaurant|
       restaurant_name = restaurant.name
       begin
@@ -22,8 +25,14 @@ class Restaurant < ActiveRecord::Base
       restaurant_rating = restaurant.rating
       restaurant_rating_img = restaurant.rating_img_url_small
       restaurant_url = restaurant.url
+      restaurant_distance = (restaurant.distance * 0.00062137).round(2)
+
+      # binding.pry
       
-      Restaurant.create(name: restaurant_name, address: restaurant_address, image_url: restaurant_image, rating: restaurant_rating, rating_img_url: restaurant_rating_img, url: restaurant_url)
+      Restaurant.create(name: restaurant_name, address: restaurant_address, image_url: restaurant_image, rating: restaurant_rating, rating_img_url: restaurant_rating_img, url: restaurant_url, distance: restaurant_distance)
     end
+
   end
+
+
 end
