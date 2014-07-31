1bdfbc31959e57536780f3c5a362e8def8fcd6e9
Vanessa Dean <vanessa.dean@gmail.com>
2014-07-31 07:52:22 -0400

    Add restaurant resource

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,14 +1,10 @@
 class MapController < ApplicationController
 
   def index
-    #@food = Yelp.client.search('San Francisco', { term: 'food' })
-    # coordinates = { latitude: 37.7577, longitude: -122.4376 }
-    # @food.client.search_by_coordinates(coordinates, params, locale)
-  end
+    Restaurant.destroy_all
+    Restaurant.get_yelp
 
-  # def search
-  #   parameters = { term: params[:term], limit: 16 }
-  #   render json: Yelp.client.search('San Francisco', parameters)
-  # end
+    @restaurants = Restaurant.all
+  end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,22 @@
 class Restaurant < ActiveRecord::Base
+    
+  def self.get_yelp
+    #parameters to be passed in search, e.g. hardcoded flatiron latlon info
+    coordinates = { latitude: 40.7055268, longitude: -74.0143461 }
+    params = {term: 'restaurant',limit: 5, sort: 1}
+    locale = {lang: 'eng'}
 
+    #searching
+    search = Yelp.client.search_by_coordinates(coordinates, params, locale) 
+
+    search.businesses.each do |restaurant|
+      restaurant_name = restaurant.name
+      restaurant_image = restaurant.image_url
+      restaurant_address = restaurant.location.display_address.join(",")
+      restaurant_rating = restaurant.rating
+      restaurant_rating_img = restaurant.rating_img_url_small
+      restaurant_url = restaurant.url
+      Restaurant.create(name: restaurant_name, address: restaurant_address, image_url: restaurant_image, rating: restaurant_rating, rating_img_url: restaurant_rating_img, url: restaurant_url)
+    end
+  end
 end
