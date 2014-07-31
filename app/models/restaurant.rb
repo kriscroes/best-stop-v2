171c3fe7a374409a6c0ed86bae2b99f72f32bffb
Vanessa Dean <vanessa.dean@gmail.com>
2014-07-31 11:56:42 -0400

    Working yelp partial

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,21 +1,28 @@
 class Restaurant < ActiveRecord::Base
     
-  def self.get_yelp
+  def self.get_yelp(lat, lon)
     #parameters to be passed in search, e.g. hardcoded flatiron latlon info
-    coordinates = { latitude: 40.7055268, longitude: -74.0143461 }
-    params = {term: 'restaurant',limit: 5, sort: 1}
+    coordinates = { latitude: lat, longitude: lon }
+    params = {term: 'restaurant',limit: 5, sort: 2}
     locale = {lang: 'eng'}
+    # sort: 1 = by distance
+    # sort: 2 = highest rated
 
     #searching
     search = Yelp.client.search_by_coordinates(coordinates, params, locale) 
 
     search.businesses.each do |restaurant|
       restaurant_name = restaurant.name
-      restaurant_image = restaurant.image_url
+      begin
+        restaurant_image = restaurant.image_url
+      rescue
+        restaurant_image = "http://i786.photobucket.com/albums/yy146/featherbrigade/cat-with-surprised-guilty-look.jpg"
+      end
       restaurant_address = restaurant.location.display_address.join(", ")
       restaurant_rating = restaurant.rating
       restaurant_rating_img = restaurant.rating_img_url_small
       restaurant_url = restaurant.url
+      
       Restaurant.create(name: restaurant_name, address: restaurant_address, image_url: restaurant_image, rating: restaurant_rating, rating_img_url: restaurant_rating_img, url: restaurant_url)
     end
   end
